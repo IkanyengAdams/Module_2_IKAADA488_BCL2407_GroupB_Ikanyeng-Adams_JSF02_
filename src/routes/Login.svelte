3bb66dce72537dc8fade5f4a2284607a791a26ce
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-07-30 13:23:37 +0200

    created the navigation button on both cart and login pages, working on my login page syling

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,11 +1,32 @@
 <script>
+  import { push } from 'svelte-spa-router';
 
+  let email = '';
+  let password = '';
+
+  function handleLogin() {
+    alert(`Logging in with email: ${email}`);
+  }
+
+  function mainPage() {
+    push('/');
+  }
 </script>
 
 <style>
-
+ 
 </style>
 
 <main>
-  <h1>Login page</h1>
+  <div class="login-container">
+    <h1>Login</h1>
+    <div class="input-group">
+      <input type="email" placeholder="Email" bind:value={email} />
+    </div>
+    <div class="input-group">
+      <input type="password" placeholder="Password" bind:value={password} />
+    </div>
+    <button class="login-button" on:click={handleLogin}>Login</button>
+    <button class="go-back-button" on:click={mainPage}>Back</button>
+  </div>
 </main>
