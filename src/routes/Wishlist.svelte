10e0f1754a42f2b3ad2c6bd7be8c68e7e19f3693
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-07-30 10:52:35 +0200

    Added a button for navigation.

--- src/routes/Wishlist.svelte
+++ src/routes/Wishlist.svelte
@@ -1,11 +1,24 @@
 <script>
+  import { push } from 'svelte-spa-router';
 
+  function mainPage() {
+    push('/');
+  }
 </script>
 
 <style>
-
+  
+  .go-back-button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    background-color: hsl(0, 9%, 69%);
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
 </style>
 
 <main>
   <h1>Wishlist page</h1>
+  <button class="go-back-button" on:click={mainPage}>Back</button>
 </main>
